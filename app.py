40205c94e395788a0ac28edeb09ceed076e50928
Holly Rothwell <hollyirothwell@gmail.com>
2021-03-19 17:59:55 +0000

    Learning Python

---
+++ app.py
@@ -0,0 +1,33 @@
+# Hello World
+# print("Hello World")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+
+# Variables
+# price = 10
+# rating = 5.1
+# name = "Kuri"
+# is_published = True
+# print(price, rating, name, is_published)
+
+# 1. Exercise
+# first_name = "John"
+# last_name = "Smith"
+# age = 20
+# existing_patient = False
+# print("Name:", first_name, last_name, "Age:", age, "Existing patient?:", existing_patient)
+
+# Receiving an input
+# your_name = input("What is your name? ")
+# print("Hello " + your_name)
+
+# 2. Exercise
+# your_name = input("Name: ")
+# favorite_colour = input("Colour: ")
+# print(your_name + "'s favorite colour is " + favorite_colour)
+
+# Type conversion
+birth_year = input("Birth year: ")
+age = 2021 - int(birth_year)
+print(age)
